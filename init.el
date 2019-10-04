ffe2309bb674cd1e62a4896830773cd876fe47c3
Karsten Beismann <git@kbeismann.com>
2019-10-05 01:01:09 +0200

    Remove broken variables

--- init.el
+++ init.el
@@ -1974,9 +1974,7 @@
 
   (add-to-list 'auto-mode-alist '("\\.[Cc][Ss][Vv]\\'" . csv-mode))
   (autoload 'csv-mode "csv-mode"
-    "Major mode for editing comma-separated value files." t)
-  (setq csv-separators (append '(";" ":" ",") csv-separators))
-  (setq csv-field-quotes (append '("'" "`") csv-field-quotes)))
+    "Major mode for editing comma-separated value files." t))
 
 
 ;;; EDIFF
