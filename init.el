411736d7f84ccb69eda0f0c4a51d38e11935d770
Karsten Beismann <git@kbeismann.com>
2019-10-07 15:46:49 +0200

    Remove Pomodoro stuff

--- init.el
+++ init.el
@@ -1160,22 +1160,6 @@
   (setq org-enforce-todo-dependencies t)
 
 
-  ;; POMODORO
-
-
-  ;; Activate the org-timer module:
-  (add-to-list 'org-modules 'org-timer)
-
-  ;; Set a default value for the timer, for example:
-  (setq org-timer-default-timer 25)
-
-  ;; Modify the org-clock-in so that a timer is started with the default
-  ;; value except if a timer is already started :
-  (add-hook 'org-clock-in-hook (lambda ()
-                                 (if (not org-timer-current-timer)
-                                     (org-timer-set-timer '(16)))))
-
-
   ;; ORG-AGENDA
 
 
