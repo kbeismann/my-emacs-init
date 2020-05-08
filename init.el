93f8acc9c0bd0222858afb51c3e4ac0bc75321cd
Karsten Beismann <git@kbeismann.com>
2020-05-08 15:12:11 +0200

    Update templates

--- init.el
+++ init.el
@@ -1962,6 +1962,20 @@
            (file org-todo-file)
            "* TODO \[\#C\] %^{Title} %^g\n:PROPERTIES:\n:CREATED: %U\n:END:\n\n:URL: %x\n\n%i\n\n"
            :empty-lines 1
+           :prepend 1)
+
+          ;; Key, name, type, target, template, options.
+          ("mm" "Meeting minutes" entry
+           (file+headline org-todo-file "UNSORTED")
+           "* TODO \[\#A\] %^{Title}  :meeting:%^g\nSCHEDULED: %(format-time-string \"<<%Y-%m-%d %a %H:%M>>\")\n:PROPERTIES:\n:CREATED: %U\n:END:\n\n- *Attendees:*\n\n  + [X] Karsten Beismann\n\n- *Agenda:*\n\n  1. ...%i\n\n - *Notes:*\n\n  + ...\n\n- *Next steps:*\n\n  + ...\n\n"
+           :empty-lines 0
+           :prepend 1)
+
+          ;; Key, name, type, target, template, options.
+          ("ms" "Stand-up" entry
+           (file+headline org-todo-file "UNSORTED")
+           "* TODO \[\#A\] %^{Title}  :meeting:%^g\nSCHEDULED: %(format-time-string \"<<%Y-%m-%d %a %H:%M>>\")\n:PROPERTIES:\n:CREATED: %U\n:END:\n\n- *Progress since last stand-up:*\n\n  1. ...%i\n\n- *Outlook:*\n\n  1. ...\n\n - *Questions/collaboration:*\n\n  + ...\n\n- *Notes:*\n\n  + ...\n\n"
+           :empty-lines 0
            :prepend 1)))
 
 
