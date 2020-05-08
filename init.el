b272e2aa76849b238fccb221ad4c00aa1fddc008
Karsten Beismann <karsten.beismann@gmail.com>
2020-05-08 15:09:03 +0200

    Adjust refile depth

--- init.el
+++ init.el
@@ -1905,8 +1905,8 @@
     :config
 
     ;; (setq org-refile-targets '((org-agenda-files :maxlevel . 3)))
-    (setq org-refile-targets '((nil :maxlevel              . 9)
-                               (org-agenda-files :maxlevel . 9))
+    (setq org-refile-targets '((nil :maxlevel              . 5)
+                               (org-agenda-files :maxlevel . 5))
           org-refile-use-outline-path 'file
           org-outline-path-complete-in-steps nil
           org-refile-allow-creating-parent-nodes 'confirm))
