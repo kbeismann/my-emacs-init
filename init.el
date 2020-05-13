2969067646f51a477a52071cd780bc2d70b03a89
Karsten Beismann <karsten.beismann@gmail.com>
2020-05-13 14:30:00 +0200

    Add additional date function
    Differentiate between date-time and date.

--- init.el
+++ init.el
@@ -635,10 +635,15 @@
   :config
 
   ;; Using the shell to insert the date.
-  (defun insert-current-date ()
+  (defun insert-current-date-time ()
     "Insert the current date and time in a standard Emacs format."
     (interactive)
     (insert (format-time-string "<%Y-%m-%d %a %H:%M>")))
+  (global-set-key (kbd "C-c d t i") 'insert-current-date-time)
+  (defun insert-current-date ()
+    "Insert the current date in a standard Emacs format."
+    (interactive)
+    (insert (format-time-string "<%Y-%m-%d %a>")))
   (global-set-key (kbd "C-c d i") 'insert-current-date)
 
   ;; Find non ASCII characters.
