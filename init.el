7679f75a2015bba7ed48d22bad3dd13548884aa7
Karsten Beismann <git@kbeismann.com>
2020-09-07 11:11:02 +0200

    Change font to Terminus

--- init.el
+++ init.el
@@ -324,13 +324,13 @@
 
   (defvar my-font-ubuntu
     ;; "--terminus-medium-r-normal--16.5-120-*-*-*-*-*-*"
-    ;; "Terminus:pixelsize=14"
-    "Hack:pixelsize=14"
+    "Terminus:pixelsize=12"
+    ;; "Hack:pixelsize=14"
     "My default font setting for Ubuntu.")
 
   (defvar my-font-family-ubuntu
-    ;; "Terminus"
-    "Hack"
+    "Terminus"
+    ;; "Hack"
     "My default font family setting for Ubuntu.")
 
   (defvar my-max-columns
