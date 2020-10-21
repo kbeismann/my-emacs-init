0d71a4efbd8a28f743c4aa8ed48119e4e19cd68d
Karsten Beismann <git@kbeismann.com>
2020-10-21 19:18:37 +0200

    Move comment to :doc

--- init.el
+++ init.el
@@ -1789,12 +1789,12 @@ https://emacs.stackexchange.com/questions/32150/how-to-add-a-timestamp-to-each-e
 
 ;; TYPIT
 
-;; This is a typing game for Emacs. In this game, you type words that are
-;; picked randomly from N most frequent words in language you're practicing,
-;; until time is up (by default it's one minute). Typit is quite similar to
-;; the "10 fast fingers" tests.
-
 (leaf typit
+  :doc " This is a typing game for Emacs. In this game, you type
+words that are picked randomly from N most frequent words in
+language you're practicing, until time is up (by default it's one
+minute).
+"
   :ensure t)
 
 
