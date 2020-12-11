52d63d37e68dc48d2f590ab5211ac01253a2084b
Karsten Beismann <git@kbeismann.com>
2020-12-11 13:34:57 +0100

    Move all templates to "My to-do list"
    Also change "Unsorted" to "UNCATEGORIZED".

--- templates.el
+++ templates.el
@@ -7,24 +7,10 @@
       '(
         ;; Key, name, type, target, template, options.
         ;; ("n" "Save Note" entry
-        ;;  (file+headline "~/gitdir/orgdir/notes.org" "Unsorted")
+        ;;  (file+headline "~/gitdir/orgdir/notes.org" "UNCATEGORIZED")
         ;;  "* TODO \[\#C\] %^{Title} %^g\n:PROPERTIES:\n:CREATED: %U\n:END:\n\n%i\n\n"
         ;;  :empty-lines 1
         ;;  :prepend 1)
-
-        ("n" "Save note" entry
-         (file+headline org-default-notes-file "Unsorted")
-         "* UNCATEGORIZED \[\#C\] %^{Title} %^g\n:PROPERTIES:\n:CREATED: %U\n:END:\n\n%i\n\n"
-         :empty-lines 1
-         :prepend 1)
-
-        ;; Key, name, type, target, template, options.
-        ("u" "Store URL" entry
-         (file+headline org-default-notes-file "Unsorted")
-         "* UNCATEGORIZED \[\#C\] %^{Title} %^g\n:PROPERTIES:\n:CREATED: %U\n:END:\n\n:URL: %x\n\n%i\n\n"
-         :empty-lines 1
-         :prepend 1)
-
         ;; Templates for my personal to-do list:
 
         ("m" "My to-do list")
@@ -33,6 +19,20 @@
         ("mt" "TODO" entry
          (file+headline org-todo-file "To-dos")
          "* TODO \[\#C\] %^{Title} %^g\n:PROPERTIES:\n:CREATED: %U\n:END:\n\n%i\n\n"
+         :empty-lines 1
+         :prepend 1)
+
+
+        ("n" "Save note" entry
+         (file+headline org-default-notes-file "UNCATEGORIZED")
+         "* UNCATEGORIZED \[\#C\] %^{Title} %^g\n:PROPERTIES:\n:CREATED: %U\n:END:\n\n%i\n\n"
+         :empty-lines 1
+         :prepend 1)
+
+        ;; Key, name, type, target, template, options.
+        ("u" "Store URL" entry
+         (file+headline org-default-notes-file "UNCATEGORIZED")
+         "* UNCATEGORIZED \[\#C\] %^{Title} %^g\n:PROPERTIES:\n:CREATED: %U\n:END:\n\n:URL: %x\n\n%i\n\n"
          :empty-lines 1
          :prepend 1)
 
@@ -52,14 +52,14 @@
 
         ;; Key, name, type, target, template, options.
         ("mm" "Meeting minutes" entry
-         (file+headline org-default-dpdhl-notes-file "Unsorted")
+         (file+headline org-default-dpdhl-notes-file "UNCATEGORIZED")
          "* TODO \[\#A\] %^{Title} :meeting:minutes:%^g\nSCHEDULED: %T\n:PROPERTIES:\n:CREATED: %U\n:END:\n\n- *Attendees:*\n\n  + [X] Karsten Beismann\n\n- *Agenda:*\n\n  1. ...%i\n\n - *Notes:*\n\n  + ...\n\n- *Next steps:*\n\n  + ...\n\n"
          :empty-lines 1
          :prepend 1)
 
         ;; Key, name, type, target, template, options.
         ("ms" "Stand-up" entry
-         (file+headline org-default-dpdhl-notes-file "Unsorted")
+         (file+headline org-default-dpdhl-notes-file "UNCATEGORIZED")
          "* TODO \[\#A\] Stand-up :meeting:standup:%^g\nSCHEDULED: %T\n:PROPERTIES:\n:CREATED: %U\n:END:\n\n- *Progress since the last meeting:*\n\n  1. ...%i\n\n- *Outlook:*\n\n  1. ...\n\n - *Questions/collaboration:*\n\n  + ...\n\n- *Notes:*\n\n  + ...\n\n"
          :empty-lines 1
          :prepend 1))
