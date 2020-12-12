599dd11dafc8538d1468a8d1da81d0aa55d9cd62
Karsten Beismann <git@kbeismann.com>
2020-12-12 09:32:07 +0100

    Add header for templates

--- templates.el
+++ templates.el
@@ -1,9 +1,36 @@
+;;; templates.el --- summary -*- lexical-binding: t -*-
+
+;; Author: Karsten Beismann
+;; Keywords: org templates
+
+
+;; This file is not part of GNU Emacs
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; Some templates for org-capture.
+
+;;; Code:
 
 ;; TODO: Clean up and restructure the templates.
+
 (setq org-capture-templates
 
       ;; Basic templates for notes and URLs:
-
       '(
         ;; Key, name, type, target, template, options.
         ;; ("n" "Save Note" entry
