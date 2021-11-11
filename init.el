d8aee9d20f632c86a35e3c922342b91ceba9a7e5
Karsten Beismann <git@kbeismann.com>
2021-11-11 15:32:53 +0100

    Show agenda for one week only

--- init.el
+++ init.el
@@ -1112,7 +1112,7 @@
      (calendar-week-start-day . 1)
      ;; (org-agenda-start-day . "-7d")
      (org-agenda-start-on-weekday . 1)
-     (org-agenda-span . 14)))
+     (org-agenda-span . 'week)))
 
   ;; Always insert blank line before headings.
   (setq org-blank-before-new-entry '((heading . auto)
