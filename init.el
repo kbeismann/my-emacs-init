ca210825cf2362e8f24dad6e8af93273aeb0ac05
Karsten Beismann <git@kbeismann.com>
2023-03-22 14:39:03 +0100

    Add `chatgpt` package

--- init.el
+++ init.el
@@ -1363,6 +1363,13 @@
 (use-package kubernetes
   :commands (kubernetes-overview))
 
+(use-package chatgpt
+  :straight (:host github :repo "joshcho/ChatGPT.el" :files ("dist" "*.el"))
+  :init
+  (require 'python)
+  (setq chatgpt-repo-path "~/.emacs.d/straight/repos/ChatGPT.el/")
+  :bind ("C-c q" . chatgpt-query))
+
 ;;; Footer:
 (provide 'init)
 ;;; init.el ends here
