9a781a67ce69bdace1eb12479471e72930e484f5
Karsten Beismann <git@kbeismann.com>
2023-05-03 15:55:17 +0200

    Add `tree-setter` sources and activate it for relevant modes

--- init.el
+++ init.el
@@ -212,6 +212,39 @@
 (prog1 "Warnings."
   (setq warning-suppress-types '((yasnippet backquote-change))))
 
+(prog1 "Setting up tree-sitter"
+  (setq treesit-language-source-alist
+        '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+          (c "https://github.com/tree-sitter/tree-sitter-c")
+          (cmake "https://github.com/uyha/tree-sitter-cmake")
+          (common-lisp "https://github.com/theHamsta/tree-sitter-commonlisp")
+          (cpp "https://github.com/tree-sitter/tree-sitter-cpp")
+          (css "https://github.com/tree-sitter/tree-sitter-css")
+          (csharp "https://github.com/tree-sitter/tree-sitter-c-sharp")
+          (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+          (go "https://github.com/tree-sitter/tree-sitter-go")
+          (go-mod "https://github.com/camdencheek/tree-sitter-go-mod")
+          (html "https://github.com/tree-sitter/tree-sitter-html")
+          (js . ("https://github.com/tree-sitter/tree-sitter-javascript" "master" "src"))
+          (json "https://github.com/tree-sitter/tree-sitter-json")
+          (lua "https://github.com/Azganoth/tree-sitter-lua")
+          (make "https://github.com/alemuller/tree-sitter-make")
+          (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+          (python "https://github.com/tree-sitter/tree-sitter-python")
+          (r "https://github.com/r-lib/tree-sitter-r")
+          (rust "https://github.com/tree-sitter/tree-sitter-rust")
+          (toml "https://github.com/tree-sitter/tree-sitter-toml")
+          (tsx . ("https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src"))
+          (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src"))
+          (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
+
+  ;; Add tree-sitter mode to relevant programming modes.  For major
+  ;; modes, e.g. Python this is done in the respective section.
+  (add-to-list 'major-mode-remap-alist '(make-mode . make-ts-mode))
+  (add-to-list 'major-mode-remap-alist '(markdown-mode . markdown-ts-mode))
+  (add-to-list 'major-mode-remap-alist '(elisp-mode . elisp-ts-mode))
+  (add-to-list 'major-mode-remap-alist '(toml-mode . toml-ts-mode)))
+
 (prog1 "File-related settings."
   (use-package no-littering
     :defer nil
@@ -759,6 +792,9 @@
   (after-init . global-flycheck-mode))
 
 (prog1 "*python-setup"
+  ;; Activate tree-sitter for Python.
+  (add-to-list 'major-mode-remap-alist '(python-mode . python-ts-mode))
+
   (setq python-indent-offset 4)
 
   (use-package conda
@@ -1308,7 +1344,9 @@
   :bind
   (:map
    yaml-mode-map
-   ("C-m" . newline-and-indent)))
+   ("C-m" . newline-and-indent))
+  :config
+  (add-to-list 'major-mode-remap-alist '(yaml-mode . yaml-ts-mode)))
 
 (use-package csv-mode
   :disabled t
@@ -1348,7 +1386,9 @@
   :config
   (helm-projectile-on))
 
-(use-package json-mode)
+(use-package json-mode
+  :config
+  (add-to-list 'major-mode-remap-alist '(json-mode . json-ts-mode)))
 
 (use-package dockerfile-mode
   :config
