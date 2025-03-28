5fedc5f6a485dde5d4b23f9b33a1fa204d430c02
Karsten Beismann <git@kbeismann.com>
2025-03-28 15:57:28 +0100

    Minor formatting

--- early-init.el
+++ early-init.el
@@ -1,45 +1,37 @@
-;;; early-init.el ---  Early initialization file -*- lexical-binding: t; coding: utf-8 -*-
-
+;;; early-init.el --- Early initialization file -*- lexical-binding: t; coding: utf-8 -*-
 
 ;; Copyright (C) 2019 Karsten E. Beismann
-
 ;; Author: Karsten Beismann
 ;; Homepage: https://github.com/kbeismann/emacs-init
 ;; Created: Tue Sep 24 21:43:39 2019 +0200
 ;; Package-Requires: ((emacs "28"))
-;; Keywords: init emacs early
-
+;; Keywords: init, emacs, early
 
 ;; This file is not part of GNU Emacs.
 
 ;; This file is free software; you can redistribute it and/or modify it under
 ;; the terms of the GNU General Public License as published by the Free
-;; Software Foundation; either version 3, or (at your option) any later
-;; version.
+;; Software Foundation; either version 3, or (at your option) any later version.
 
 ;; This program is distributed in the hope that it will be useful, but WITHOUT
 ;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-;; FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+;; FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
 ;; more details.
 
 ;; For a full copy of the GNU General Public License see
 ;; <http://www.gnu.org/licenses/>.
 
-
 ;;; Commentary:
 
-;; This file is used for running early initializations before init.el.  This
-;; feature was introduced with Emacs 27.
-
+;; This file handles early initialization settings for Emacs, introduced in Emacs 27.
 
 ;;; Code:
 
-(prog1 "Show startup time."
-  (add-hook 'emacs-startup-hook
-            (lambda ()
-              (message "Emacs loaded in %s seconds with %d garbage collections."
-                       (emacs-init-time)
-                       gcs-done))))
+;; Show startup time after initialization
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (message "Emacs loaded in %s seconds with %d garbage collections."
+                     (emacs-init-time) gcs-done)))
 
 (prog1
     "Make use of the native compilation system introduced in emacs 28."
@@ -56,21 +48,15 @@
         (setq comp-deferred-compilation t))
     (message "Native compilation is *not* available.")))
 
-(prog1
-    "Check if fast JSON is available."
-  (if (functionp 'json-serialize)
-      (message "Native JSON is available.")
-    (message "Native JSON is *not* available.")))
+;; Check for native JSON support and report availability
+(message "Native JSON is %savailable."
+         (if (functionp 'json-serialize) "" "not "))
 
-;; Increase data read from the processess (4MB).
+;; Increase the maximum data read from processes to optimize performance
 (setq read-process-output-max (* 1024 1024 4))
 
-
-;; This block effectively disables garbage collection for the initialization
-;; time and re-enables it after.  If the machine has enough RAM, at most 64MB
-;; every time you start up a new Emacs, this will reduce package-initialize
-;; time to about half.
-
+;; Disable garbage collection during initialization for faster startup
+;; Re-enable it after startup when Emacs is ready
 (prog1 "More generous `gc-cons-threshold' value."
   (setq garbage-collection-messages t)
   (defvar original-gc-cons-threshold gc-cons-threshold)
@@ -81,7 +67,8 @@
      (setq gc-cons-threshold better-gc-cons-threshold)
      (setq file-name-handler-alist file-name-handler-alist-original)
      (makunbound 'file-name-handler-alist-original)))
-  ;; Auto GC.
+
+  ;; Auto-trigger garbage collection after focus change
   (add-hook
    'emacs-startup-hook
    (lambda ()
@@ -94,64 +81,62 @@
        (add-hook
         'after-focus-change-function
         'garbage-collect))
+
+     ;; Setup hooks for GC during minibuffer interaction
      (defun gc-minibuffer-setup-hook ()
        (setq gc-cons-threshold most-positive-fixnum))
      (defun gc-minibuffer-exit-hook ()
        (garbage-collect)
        (setq gc-cons-threshold better-gc-cons-threshold))
+
      (add-hook 'minibuffer-setup-hook #'gc-minibuffer-setup-hook)
      (add-hook 'minibuffer-exit-hook #'gc-minibuffer-exit-hook))))
 
-;; Let's increase the max-lisp-eval-depth and max-specpdl-size to
-;; prevent exceeding recursion limits.
+;; Increase recursion limits to avoid exceeding maximum recursion depth
 (setq max-lisp-eval-depth 50000)
 (setq max-specpdl-size 10000)
 
-;; Disable certain byte compiler warnings to cut down on the noise.
-(setq byte-compile-warnings '(not cl-functions obsolete));; Defer GC.
+;; Disable specific byte compiler warnings to reduce noise
+(setq byte-compile-warnings '(not cl-functions obsolete))
+
+;; Defer garbage collection during initialization
 (setq gc-cons-threshold most-positive-fixnum
       gc-cons-percentage 0.6)
 
-;; Unset PES
+;; Disable package loading at startup
 (setq package-enable-at-startup nil)
 
-;; Unset FNHA
+;; Temporarily disable file name handlers to speed up loading
 (defvar file-name-handler-alist-original file-name-handler-alist)
 (setq file-name-handler-alist nil)
 
-;; Unset SRF
+;; Disable site-run-file to avoid unnecessary startup operations
 (setq site-run-file nil)
 
-
-;; When using Emacs 29, the location of the native compilation cache
-;; can be changed using a function, preferably in early-init.el.  The
-;; path is related to the default directory structure of the package
-;; no-littering.  The respective settings are in init.el.  Source:
-;; https://github.com/emacscollective/no-littering#native-compilation-cache
+;; Configure the location of the native compilation cache (Emacs 29+)
+;; This is related to the `no-littering` package and can be set here
 (when (boundp 'native-comp-eln-load-path)
   (startup-redirect-eln-cache
-   (convert-standard-filename
-	(expand-file-name  "var/eln-cache/" user-emacs-directory))))
+   (expand-file-name "var/eln-cache/" user-emacs-directory)))
 
-;; Some visual simplifications.
+;; UI optimizations: hide unnecessary UI elements for a cleaner experience
+(setq-default menu-bar-mode nil
+              tool-bar-mode nil
+              scroll-bar-mode nil
+              cursor-type 'bar
+              frame-inhibit-implied-resize t
+              inhibit-compacting-font-caches t
+              use-dialog-box nil)
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
 
-(setq scroll-bar-mode nil)
-(setq menu-bar-mode nil)
-(setq tool-bar-mode nil)
-(setq cursor-type 'bar)
-
-(setq frame-inhibit-implied-resize t)
-(setq inhibit-compacting-font-caches t)
-(setq use-dialog-box t)
-
-;; Coding system.
+;; Set preferred encoding to UTF-8
 (prefer-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 
+;; OS and font-related settings based on the system type
 (prog1 "OS- and font-related settings."
   (defvar checkos0  "Checking OS and hostname...")
   (defvar font0 "Looking for font family...")
@@ -185,12 +170,12 @@
     ;; "Terminus"
     "Hack"
     "My default font family setting for Ubuntu.")
+
   (progn
     (message checkos0)
     (if (eq system-type 'gnu/linux)
         (progn
-          (message
-           (concat checkos0 "done"))
+          (message (concat checkos0 "done"))
           (defvar my-os
             (substring
              (shell-command-to-string "lsb_release -sd")
@@ -201,97 +186,51 @@
              (shell-command-to-string "hostname")
              0 -1))
           (message "Found hostname: %s" my-hostname)
-          ;; Font for Huckleberry.
-          (if (string-equal "huckleberry"
-                            (substring my-hostname 0 11))
+
+          ;; Font setup for Huckleberry
+          (if (string-equal "huckleberry" (substring my-hostname 0 11))
               (progn
                 (message "Current font settings for Huckleberry: %s"
                          my-font-huckleberry)
                 (message font0)
                 (if (and
-                     (null (string= ""
-                                    (shell-command-to-string
-                                     "which fc-list")))
-                     (null (string= ""
-                                    (shell-command-to-string
-                                     (concat
-                                      "fc-list "
-                                      my-font-family-huckleberry)))))
+                     (null (string= "" (shell-command-to-string "which fc-list")))
+                     (null (string= "" (shell-command-to-string (concat "fc-list " my-font-family-huckleberry)))))
                     (progn
-                      (message
-                       (concat font0 "done"))
-                      (message "Font installed: %s"
-                               my-font-family-huckleberry)
-                      (add-to-list 'default-frame-alist
-                                   `(font . ,my-font-huckleberry)))
-                                        ; Works for emacsclient as well.
-                  (message "Missing font family: %s" my-font-family-huckleberry)))
-            ;; Font for Arch.
-            (if (string-equal "Arch"
-                              (substring my-os 1 5))
+                      (message (concat font0 "done"))
+                      (message "Font installed: %s" my-font-family-huckleberry)
+                      (add-to-list 'default-frame-alist `(font . ,my-font-huckleberry)))))
+            ;; Font setup for Arch
+            (if (string-equal "Arch" (substring my-os 1 5))
                 (progn
-                  (message "Current font settings for Arch Linux: %s"
-                           my-font-arch)
+                  (message "Current font settings for Arch Linux: %s" my-font-arch)
                   (message font0)
                   (if (and
-                       (null
-                        (string= ""
-                                 (shell-command-to-string
-                                  "which fc-list")))
-                       (null
-                        (string= ""
-                                 (shell-command-to-string
-                                  (concat
-                                   "fc-list "
-                                   my-font-family-arch)))))
+                       (null (string= "" (shell-command-to-string "which fc-list")))
+                       (null (string= "" (shell-command-to-string (concat "fc-list " my-font-family-arch)))))
                       (progn
-                        (message
-                         (concat font0 "done"))
-                        (message "Font installed: %s"
-                                 my-font-family-arch)
+                        (message (concat font0 "done"))
+                        (message "Font installed: %s" my-font-family-arch)
                         (message font1)
-                        (add-to-list 'default-frame-alist
-                                     `(font . ,my-font-arch))
-                                        ; Works for emacsclient as well.
-                        (message
-                         (concat font1 "done")))
-                    (message "Missing font family: %s" my-font-family-arch)))
-              ;; Font for Ubuntu.
-              (if (string-equal
-                   (substring my-os 0 5)
-                   (substring "Ubuntu" 0 5))
+                        (add-to-list 'default-frame-alist `(font . ,my-font-arch)))))
+              ;; Font setup for Ubuntu
+              (if (string-equal (substring my-os 0 5) (substring "Ubuntu" 0 5))
                   (progn
                     (message "Current font settings for Ubuntu: %s" my-font-ubuntu)
                     (message font1)
                     (if (and
-                         (null
-                          (string= ""
-                                   (shell-command-to-string
-                                    "which fc-list")))
-                         (null
-                          (string= ""
-                                   (shell-command-to-string
-                                    (concat
-                                     "fc-list "
-                                     my-font-family-ubuntu)))))
+                         (null (string= "" (shell-command-to-string "which fc-list")))
+                         (null (string= "" (shell-command-to-string (concat "fc-list " my-font-family-ubuntu)))))
                         (progn
-                          (message "Font installed: %s"
-                                   my-font-family-ubuntu)
+                          (message "Font installed: %s" my-font-family-ubuntu)
                           (message font1)
-                          (add-to-list 'default-frame-alist
-                                       `(font . ,my-font-ubuntu))
-                          (message
-                           (concat font1 "done")))
-                                        ; Works for emacsclient as well.
-                      (message "Missing font family: %s" my-font-family-ubuntu))
-                    (message "Adjusting frame parameters...")
-                    (add-to-list 'default-frame-alist
-                                 '(height . 50))
-                    (add-to-list 'default-frame-alist
-                                 '(width . 180))
-                    (message "Adjusting frame parameters...done"))
-                (message "No predefined font settings found")))))
-      (message "No Linux-based system found > font settings are not applicable"))))
+                          (add-to-list 'default-frame-alist `(font . ,my-font-ubuntu)))))
+                (message "Adjusting frame parameters...")
+                (add-to-list 'default-frame-alist '(height . 50))
+                (add-to-list 'default-frame-alist '(width . 180))
+                (message "Adjusting frame parameters...done"))
+              (message "No predefined font settings found")))))
+    (message "No Linux-based system found > font settings are not applicable")))
 
 (provide 'early-init)
 ;;; early-init.el ends here
