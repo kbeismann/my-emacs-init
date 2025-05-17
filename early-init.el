accd1fa3e94924e1ea060b0ada4e3fafea2b8ba1
Karsten Beismann <git@kbeismann.com>
2025-05-17 19:24:52 +0200

    Update license header in init files
    Update the license header in `early-init.el` and `init.el` to use a standard GNU
    GPL v3 format.

--- early-init.el
+++ early-init.el
@@ -6,19 +6,20 @@
 ;; Homepage: https://github.com/kbeismann/emacs-init
 ;; Created: Tue Sep 24 21:43:39 2019 +0200
 
-;; This file is not part of GNU Emacs.
-
-;; This file is free software; you can redistribute it and/or modify it under
-;; the terms of the GNU General Public License as published by the Free
-;; Software Foundation; either version 3, or (at your option) any later version.
-
+;; This file is part of my Emacs configuration.
+;;
+;; My Emacs configuration is free software: you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at your
+;; option) any later version.
+;;
 ;; This program is distributed in the hope that it will be useful, but WITHOUT
-;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-;; FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
-;; more details.
-
-;; For a full copy of the GNU General Public License see
-;; <http://www.gnu.org/licenses/>.
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+;; FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+;; details.
+;;
+;; You should have received a copy of the GNU General Public License along with
+;; this program. If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
