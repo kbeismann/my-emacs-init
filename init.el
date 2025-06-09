d197934cb2f57d61eb57e0876e9ecbf4e6e480e5
Karsten Beismann <git@kbeismann.com>
2025-06-09 23:35:18 +0200

    Move package management settings to separate file
    Move package archive settings, `package-initialize`, `straight.el`,
    `use-package`, `auto-compile`, and `benchmark-init` configuration from
    `init.el` to the new file `lisp/package-management.el`. Add `(require
    'package-management)` to `init.el` to load the relocated configuration.
    This centralizes package manager setup and related utilities.

--- init.el
+++ init.el
@@ -30,6 +30,8 @@
 
 (add-to-list 'load-path "~/.emacs.d/lisp")
 
+(require 'package-management)
+
 ;; Load work-related settings.
 (let ((work-projects (expand-file-name "projects.el" user-emacs-directory)))
   (cond
@@ -38,60 +40,6 @@
     (load-file work-projects))
    (t
     (message "No project-related settings found."))))
-
-;; Package archives.
-(setq package-check-signature 'allow-unsigned)
-(setq package-archives
-      '(("gnu" . "https://elpa.gnu.org/packages/")
-        ("org" . "https://orgmode.org/elpa/")
-        ("melpa" . "https://melpa.org/packages/")
-        ("melpa-stable" . "https://stable.melpa.org/packages/")))
-(setq package-archive-priorities
-      '(("gnu" . 2) ("org" . 1) ("melpa" . 3) ("melpa-stable" . 0)))
-
-;; Initialize package BEFORE loading use-package and straight.
-(package-initialize)
-
-;; Bootstrap and set up straight.el package manager.
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el"
-                         user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent
-         'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
-
-(setq straight-check-for-modifications nil)
-(setq straight-vc-git-default-clone-depth 1)
-(setq straight-use-package-by-default t)
-(setq use-package-always-defer t)
-
-(straight-use-package 'use-package)
-
-(require 'use-package-ensure)
-(setq use-package-always-ensure t)
-
-;; Disable specific byte compiler warnings to reduce noise.
-(setq byte-compile-warnings '(not cl-functions obsolete))
-
-(use-package
- auto-compile
- :init (setq load-prefer-newer t)
- :config
- (auto-compile-on-load-mode)
- (auto-compile-on-save-mode))
-
-(use-package
- benchmark-init
- :init (benchmark-init/activate)
- :hook (after-init . benchmark-init/deactivate))
 
 (use-package shfmt)
 
