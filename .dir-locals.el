9c485c0a9081799fb46b042416e9831b21157ce8
Karsten Beismann <git@kbeismann.com>
2025-10-14 21:48:09 +0200

    Remove trailing whitespace via `.dir-locals.el`
    Update `.dir-locals.el` to automatically remove trailing whitespace from
    all files before saving.
    
    A new entry for `nil` mode adds `delete-trailing-whitespace` to the
    local `before-save-hook`. This change enforces a consistent style and
    improves codebase cleanliness across the entire project.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,6 @@
-((emacs-lisp-mode
+((nil
+  . ((eval . (add-hook 'before-save-hook #'delete-trailing-whitespace nil t))))
+ (emacs-lisp-mode
   .
   ((elisp-autofmt-empty-line-max . 1)
    (eval . (add-hook 'emacs-lisp-mode-hook #'elisp-autofmt-mode nil 'local)))))
