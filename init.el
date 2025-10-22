2f7a9abec183d5259cead65ea4406317c614b942
Karsten Beismann <git@kbeismann.com>
2025-10-22 11:36:42 +0200

    Add WSL clipboard and file system integration
    Introduce `lisp/wsl.el` to provide enhanced integration for Emacs
    running within Windows Subsystem for Linux (WSL). This module is loaded
    by `init.el` and sets up several key functionalities.
    
    It configures Emacs to use `clip.exe` for `interprogram-cut-function`
    and `powershell.exe` for `interprogram-paste-function`, enabling
    seamless copy-pasting between Emacs in WSL and Windows applications. The
    necessary paths for these executables are added to `exec-path`.
    
    A new command `open-downloads-folder` is provided to quickly open the
    Windows Downloads directory in `dired`. This facilitates easier file
    management across the WSL/Windows boundary.
    
    To ensure the custom clipboard functions are utilized,
    `x-select-enable-clipboard` and `x-select-enable-primary` are set to
    `nil` when running in a terminal environment, overriding default X
    clipboard behavior.
    
    This change significantly improves the user experience by bridging the
    gap between Emacs in WSL and the native Windows environment.

--- init.el
+++ init.el
@@ -48,7 +48,8 @@ Your current Emacs version is %s."
    (t
     (message "No project-related settings found."))))
 
-(let ((wsl-functions (expand-file-name "wsl.el" user-emacs-directory)))
+(let ((wsl-functions
+       (expand-file-name "wsl.el" (concat user-emacs-directory "lisp/"))))
   (cond
    ((file-exists-p wsl-functions)
     (message "Found WSL-related settings...")
