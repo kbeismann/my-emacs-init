bf2740fabca62e19441daedc316d1a37ed75a894
Karsten Beismann <git@kbeismann.com>
2025-11-03 10:42:01 +0100

    Move `mc/always-run-for-all` to `multiple-cursors` `:init`
    Relocate `mc/always-run-for-all` from the `:config` block to the `:init`
    block of the `multiple-cursors` `use-package` declaration in `init.el`.
    
    This ensures the variable is set earlier during package initialization,
    which is a more conventional placement for such a global setting and
    simplifies the `use-package` structure.

--- init.el
+++ init.el
@@ -504,13 +504,12 @@ Your current Emacs version is %s."
 ;; Basic bindings for multiple-cursors.
 (use-package
  multiple-cursors
- :init (setq warning-suppress-types '((files)))
+ :init (setq warning-suppress-types '((files))) (setq mc/always-run-for-all t)
  :bind*
  (("C-S-c C-S-c" . mc/edit-lines)
   ("C->" . mc/mark-next-like-this)
   ("C-<" . mc/mark-previous-like-this)
-  ("C-M->" . mc/mark-all-like-this))
- :config (setq mc/always-run-for-all t))
+  ("C-M->" . mc/mark-all-like-this)))
 
 (use-package
  flycheck
