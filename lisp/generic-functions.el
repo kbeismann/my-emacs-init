9c3e63cb660cb5246156de286cd69a174b6e385f
Karsten Beismann <git@kbeismann.com>
2026-02-11 14:16:43 +0100

    Add `my/update-pr` function to `generic-functions.el`
    Define `my/update-pr` to execute the `update_pr` shell command within an
    interactive Bash shell and bind it to `C-c u p`. This enables updating
    pull requests directly from Emacs while ensuring the shell environment
    is correctly sourced.

--- lisp/generic-functions.el
+++ lisp/generic-functions.el
@@ -130,5 +130,14 @@
              directory
              processed-count)))
 
+(defun my/update-pr ()
+  "Run the update_pr command in a shell that sources ~/.bashrc."
+  (interactive)
+  (let ((exit-code (shell-command "bash -i -c 'update_pr'")))
+    (if (= exit-code 0)
+        (message "Pull request updated successfully.")
+      (message "Failed to update pull request. Exit code: %d" exit-code))))
+(define-key global-map (kbd "C-c u p") 'my/update-pr)
+
 (provide 'generic-functions)
 ;;; generic-functions.el ends here
